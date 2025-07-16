ed50caa01051f52f443c80b28e048279c921edd2
mux-mux <kobylyukh@gmail.com>
2025-07-16 16:03:48 +0300

    add initial project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import * as tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vite.dev/config/
+const VENDOR_LIBS = ['/react/', '/react-dom/', '/react-router/', '/lodash/'];
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        tsconfigPaths.default(),
+        visualizer({
+            filename: 'bundle-stats.html',
+            open: false,
+            gzipSize: true,
+            brotliSize: true,
+            emitFile: true,
+        }),
+    ],
+    server: {
+        open: true,
+        port: 3000,
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (
+                        id.includes('node_modules') &&
+                        VENDOR_LIBS.some((lib) => id.includes(lib))
+                    ) {
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
+});
