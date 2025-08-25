bc6a6b5c3d296e648de4cc971be504cbae7e6a91
mux-mux <kobylyukh@gmail.com>
2025-08-25 13:18:06 +0300

    fix createRoot element typing

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './main.css';
 import App from './App';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
         <App />
     </StrictMode>,
