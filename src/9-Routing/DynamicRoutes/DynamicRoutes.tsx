794e5ad9ed2312523fb239b3bfb3606254ed7836
mux-mux <kobylyukh@gmail.com>
2025-08-29 17:42:47 +0300

    add dynamic routes chapter

---
+++ src/9-Routing/DynamicRoutes/DynamicRoutes.tsx
@@ -0,0 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ChapterWrapper } from 'components/ChapterWrapper/ChapterWrapper';
+import { StyledLink } from 'components/StyledLink/StyledLink';
+import { Outlet } from 'react-router';
+
+const queryClient = new QueryClient();
+
+export function DynamicRoutes() {
+    return (
+        <ChapterWrapper
+            title="Dynamic routes"
+            subtitle="Nested routes, dynamic segments & useParams()"
+        >
+            <StyledLink to="breeds">Breed List</StyledLink>
+            <QueryClientProvider client={queryClient}>
+                <Outlet />
+            </QueryClientProvider>
+        </ChapterWrapper>
+    );
+}
