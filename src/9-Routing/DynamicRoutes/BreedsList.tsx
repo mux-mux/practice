bcc680445e73ec23042aaf46860d6aa3e0bcd12b
mux-mux <kobylyukh@gmail.com>
2025-08-29 17:55:07 +0300

    add query breeds

---
+++ src/9-Routing/DynamicRoutes/BreedsList.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
+import classes from './BreedsList.module.css';
+import { StyledLink } from 'components/StyledLink/StyledLink';
+import { Outlet } from 'react-router';
+
+export function BreedsList() {
+    const { data } = useBreeds();
+
+    return (
+        <div className={classes.root}>
+            <div className={classes.list}>
+                {data?.message &&
+                    Object.keys(data.message).map((breed) => (
+                        <StyledLink key={breed} to={breed}>
+                            {breed}
+                        </StyledLink>
+                    ))}
+            </div>
+            <div>
+                <Outlet />
+            </div>
+        </div>
+    );
+}
+
+type BreedsListResponse = {
+    message: Record<string, string[]>;
+};
+
+function useBreeds() {
+    return useQuery<BreedsListResponse>({
+        queryKey: ['breeds', 'lise'],
+        queryFn: async () => {
+            const response = await fetch('https://dog.ceo/api/breeds/list/all');
+            return response.json();
+        },
+    });
+}
