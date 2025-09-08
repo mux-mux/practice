bbdc54d04744e042e3d367b44519adb59b2cae95
mux-mux <kobylyukh@gmail.com>
2025-09-08 18:57:02 +0300

    add useFetch hook

--- src/14-Challanges/01-Pagination.tsx
+++ src/14-Challanges/01-Pagination.tsx
@@ -1,3 +1,18 @@
+import { useFetch } from '@/hooks/useFetch';
+
+type Post = {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+};
+
 export function Pagination() {
-    return <h1 className="text-3xl">Pagination App</h1>;
+    const { data } = useFetch<Post[]>('https://jsonplaceholder.typicode.com/posts');
+    console.log(data);
+    return (
+        <div>
+            <h1 className="text-3xl">Pagination App</h1>
+        </div>
+    );
 }
