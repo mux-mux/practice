d027d366df0fb393fc951b9930e5b790662921f8
mux-mux <kobylyukh@gmail.com>
2025-09-09 14:17:15 +0300

    add pagination app layout UI

--- src/14-Challanges/01-Pagination.tsx
+++ src/14-Challanges/01-Pagination.tsx
@@ -9,10 +9,28 @@ type Post = {
 
 export function Pagination() {
     const { data } = useFetch<Post[]>('https://jsonplaceholder.typicode.com/posts');
-    console.log(data);
+
     return (
-        <div>
-            <h1 className="text-3xl">Pagination App</h1>
+        <div className="min-h-full">
+            <div className="mx-auto max-w-3xl px-2 border border-gray-200 rounded-xl">
+                <h1 className="text-3xl text-center font-semibold">Posts Pagination</h1>
+                <div className="flex flex-col space-y-4">
+                    {data?.map(({ id, title, body }) => (
+                        <div key={id} className="border border-gray-200 rounded">
+                            <h2 className="text-xl font-semibold">{title}</h2>
+                            <p className="pt-4">{body}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className="flex justify-between">
+                    <div>`Page 1 of 10`</div>
+                    <div>
+                        <button>Previuos</button>
+                        <div>...buttons</div>
+                        <button>Next</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
