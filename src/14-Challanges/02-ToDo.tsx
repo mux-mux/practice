f6438996b76ec4a3dcb27c7b5130ff714dfadce8
mux-mux <kobylyukh@gmail.com>
2025-09-11 17:24:20 +0300

    add radix-ui config

--- src/14-Challanges/02-ToDo.tsx
+++ src/14-Challanges/02-ToDo.tsx
@@ -1,3 +1,10 @@
+import { Flex, Button, TextField } from '@radix-ui/themes';
+
 export function ToDo() {
-    return <div>ToDo</div>;
+    return (
+        <Flex justify="center" gap="2">
+            <TextField.Root placeholder="Add a task…" />
+            <Button>Add</Button>
+        </Flex>
+    );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Theme } from '@radix-ui/themes';
 import './main.css';
 import App from './App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
-        <App />
+        <Theme>
+            <App />
+        </Theme>
     </StrictMode>,
 );
